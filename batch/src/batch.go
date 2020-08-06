e5286ba10d0ff4296e1636036d6aed5d099d74a6
kyl <kang@kyldeMacBook-Pro.local>
2020-08-06 18:02:02 +0800

    batch: add batchCh
    batchCh is good for multi goroutines process

--- batch/src/batch.go
+++ batch/src/batch.go
@@ -7,28 +7,30 @@ import (
 )
 
 type Batch struct {
-	enable          bool // 是否可用
-	inputCh         chan interface{}
-	batchCount      int
-	pollingInterval time.Duration
-	process         func([]interface{}) error
-	batchItems      []interface{}
-	ctx             context.Context
-	cancel          func()
-	wgPolling       sync.WaitGroup
+	enable            bool // 是否可用
+	inputCh           chan interface{}
+	batchCount        int
+	pollingInterval   time.Duration
+	process           func([]interface{}) error
+	batchItems        []interface{}
+	ctx               context.Context
+	cancel            func()
+	wgPolling         sync.WaitGroup
+	processConcurrent int
 }
 
-func NewBatch(batchCount int, pollingInterval time.Duration, process func([]interface{}) error) *Batch {
+func NewBatch(batchCount int, pollingInterval time.Duration, process func([]interface{}) error, processConcurrent int) *Batch {
 	ctx, cancel := context.WithCancel(context.Background())
 	r := Batch{
-		enable:          true,
-		inputCh:         make(chan interface{}, 100),
-		batchCount:      batchCount,
-		pollingInterval: pollingInterval,
-		process:         process,
-		ctx:             ctx,
-		cancel:          cancel,
-		wgPolling:       sync.WaitGroup{},
+		enable:            true,
+		inputCh:           make(chan interface{}, 100),
+		batchCount:        batchCount,
+		pollingInterval:   pollingInterval,
+		process:           process,
+		ctx:               ctx,
+		cancel:            cancel,
+		wgPolling:         sync.WaitGroup{},
+		processConcurrent: processConcurrent,
 	}
 
 	r.wgPolling.Add(1)
@@ -47,24 +49,52 @@ func (b *Batch) Stop() {
 }
 
 func (b *Batch) polling() {
-	defer b.wgPolling.Done()
+	batchCh := make(chan []interface{}, 100)
+	addToBatch := func() {
+		if len(b.batchItems) > 0 {
+			batchCh <- b.batchItems
+			b.batchItems = []interface{}{}
+		}
+	}
 
+	go func() {
+		defer b.wgPolling.Done()
+
+		wg := sync.WaitGroup{}
+		for i := 0; i < b.processConcurrent; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for items := range batchCh {
+					b.process(items)
+				}
+			}()
+		}
+		wg.Wait()
+	}()
+
+	stopped := false
 	ticker := time.NewTicker(b.pollingInterval)
 	for {
 		select {
 		case item, ok := <-b.inputCh:
 			if !ok {
+				b.inputCh = nil
+				addToBatch()
+				close(batchCh)
 				return
 			}
 			b.batchItems = append(b.batchItems, item)
 			if len(b.batchItems) >= b.batchCount {
-				b.process(b.batchItems)
+				addToBatch()
 			}
 		case <-ticker.C:
-			b.process(b.batchItems)
+			addToBatch()
 		case <-b.ctx.Done():
-			close(b.inputCh)
-			return
+			if !stopped {
+				close(b.inputCh)
+				stopped = true
+			}
 		}
 	}
 }
--- batch/src/batch_test.go
+++ batch/src/batch_test.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
-var count = 0
+var count int32 = 0
+var b = NewBatch(100, time.Second, func(i []interface{}) error {
+	atomic.AddInt32(&count, int32(len(i)))
+	time.Sleep(time.Millisecond)
+	return nil
+}, 100)
 
 func TestBatch_Add(t *testing.T) {
-	b := NewBatch(100, time.Second, func(i []interface{}) error {
-		count += len(i)
-		return nil
-	})
-
 	concurrent := runtime.NumCPU()
 	amount := 1000000
 	wg := sync.WaitGroup{}
@@ -34,6 +35,32 @@ func TestBatch_Add(t *testing.T) {
 	wg.Wait()
 	b.Stop()
 
-	fmt.Printf("concurrent=%d, amount per goroutine=%d, elaspsed: %fs\n", concurrent, amount, time.Since(start).Seconds())
+	fmt.Printf("concurrent=%d, amount per goroutine=%d, elaspsed: %fs, processed: %d\n", concurrent, amount, time.Since(start).Seconds(), count)
+	time.Sleep(time.Second)
+	if int(count) != concurrent*amount {
+		t.Errorf("processed count is %d, should be %d", count, concurrent*amount)
+	}
 	// concurrent=4, amount per goroutine=1000000, elaspsed: 4.245187s
+}
+
+func TestBatch_Add_Small_Count(t *testing.T) {
+	concurrent := 2
+	amount := 101
+	wg := sync.WaitGroup{}
+
+	start := time.Now()
+	for i := 0; i < concurrent; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < amount; j++ {
+				b.Add(j)
+			}
+		}()
+	}
+
+	wg.Wait()
+	b.Stop()
+
+	fmt.Printf("concurrent=%d, amount per goroutine=%d, elaspsed: %fs, processed: %d\n", concurrent, amount, time.Since(start).Seconds(), count)
 }
